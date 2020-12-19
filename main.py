0526a7a1e32a419e65e9e93d31ea7e10ec34e4a7
FukeKazki <kazkichi0906@gmail.com>
2020-12-19 23:20:43 +0900

    Update: line_botのルーティングを追加 (#15)

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
+from line_bot.bot import broadcast
+
+import json
 
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route('/test', methods=['GET'])
 def test():
-    return 'Hello World'
+	return 'Hello World'
+
+
+@app.route('/line_bot', methods=['POST'])
+def line_bot():
+	data = request.data.decode('utf-8')
+	data = json.loads(data)
+	image_url = data['url']
+	broadcast(image_url)
+	return '200'
+
 
 app.run()
