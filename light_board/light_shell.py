ba180bca93b6f8bdcb4a7736573c056983830c9b
maekawamikako <mikankounya@gmail.com>
2020-12-19 16:40:28 +0900

    f戻した

--- light_board/light_shell.py
+++ light_board/light_shell.py
@@ -1,5 +1,5 @@
 def light_shell(text):
-    command = "sudo ./rpi-rgb-led-matrix/examples-api-use/scrolling-text-example --led-no-hardware-pulse --led-rows=16 --led-cols=32 -s 4 -f ./rpi-rgb-led-matrix/fonts/helvR12.bdf -C 102,179,22 {text}"
+    command = f"sudo ./rpi-rgb-led-matrix/examples-api-use/scrolling-text-example --led-no-hardware-pulse --led-rows=16 --led-cols=32 -s 4 -f ./rpi-rgb-led-matrix/fonts/helvR12.bdf -C 102,179,22 {text}"
     completed = subprocess.run(command) # commandがコマンド、argumentが引数。必要なだけ並べる。
 
 light_shell('Hello World')
