a5a245c3c227cb7517813787fb68d49c9ba4885a
Futaba-Kosuke <k.futaba2828@gmail.com>
2020-12-20 01:52:07 +0900

    Add: Raspberry Pi との通信処理を実装 (#4)

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
 import argparse
+import requests
 
 from flask import Flask, request
 from flask_cors import CORS
@@ -10,16 +12,26 @@ app = Flask(__name__)
 CORS(app)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-pi', '--raspberry-pi', required=True)
+parser.add_argument('-pi', '--raspberry_pi', required=True)
 parser.add_argument('-dr', '--drone', required=True)
 
 args = parser.parse_args()
-raspi_url = args.raspberry-pi
+raspi_url = args.raspberry_pi
 drone_url = args.drone
 
 @app.route('/test', methods=['GET'])
 def test():
 	return 'Hello World'
+
+@app.route('/controller', methods=['POST'])
+def controller():
+    data_json = request.data.decode('utf-8')
+    data_dict = json.loads(data_json)
+
+    access_cnt = int(data_dict['access_cnt'])
+
+    raspi_payload_str = str(access_cnt)
+    requests.get(os.path.join(raspi_url, 'light_board', raspi_payload_str))
 
 @app.route('/line_bot', methods=['POST'])
 def line_bot():
