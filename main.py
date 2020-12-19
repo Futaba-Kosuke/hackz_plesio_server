20d21cc3ac3cc84fb97da4d8ff972382c6679123
Futaba-Kosuke <k.futaba2828@gmail.com>
2020-12-20 01:29:47 +0900

    Add: 各端末のURLをコマンドライン引数で指定 (#4) (#5)

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import argparse
+
 from flask import Flask, request
 from flask_cors import CORS
 from line_bot.bot import broadcast
@@ -7,11 +9,17 @@ import json
 app = Flask(__name__)
 CORS(app)
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-pi', '--raspberry-pi', required=True)
+parser.add_argument('-dr', '--drone', required=True)
+
+args = parser.parse_args()
+raspi_url = args.raspberry-pi
+drone_url = args.drone
 
 @app.route('/test', methods=['GET'])
 def test():
 	return 'Hello World'
-
 
 @app.route('/line_bot', methods=['POST'])
 def line_bot():
@@ -20,6 +28,5 @@ def line_bot():
 	image_url = data['url']
 	broadcast(image_url)
 	return '200'
-
 
 app.run()
